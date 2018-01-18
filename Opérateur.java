68395707193d8a2660c5bb0c25187ea4bc9b704c
yukii <32541696+yukii@users.noreply.github.com>
2018-01-18 10:53:56 +0100

    Update Opérateur.java

--- Opérateur.java
+++ Opérateur.java
@@ -1,19 +1,24 @@
-public class Opérateur {
+public class OpÃ©rateur {
 	public static int addition (int x, int y){
 		System.out.println(x + y);
+		return x + y;
 	}
 	public static int soustraction (int x, int y){
 		System.out.println(x - y);
+		retrun x - y;
 	}
-	public static void multiplication (int x, int y){
+	public static int multiplication (int x, int y){
 		System.out.println(x * y);
+		return x * y;
 	}
-	public static void division (int x, int y){
+	public static int division (int x, int y){
 		if (y == 0){
-			System.out.println("Il est impossible de divisé par 0");
+			System.out.println("Il est impossible de divisÃ© par 0");
+			return null;
 		}
 		else {
 			System.out.println(x / y);
+			return x/y;
 		}
 	}
 }
